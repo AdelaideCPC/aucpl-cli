8feba5a28e9a0018df7af8c7ad8f1d3089330dc0
Ray Okamoto <git@rayokamoto.com>
2026-04-29 17:09:31 +0930

    chore(nix): add shell hook for aucpl shellinit

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,9 +1,18 @@
 {pkgs ? import <nixpkgs> {}}: let
   inherit (pkgs) lib;
+  aucpl-dev = pkgs.writeShellScriptBin "aucpl" ''
+    if [ -z "''${AUCPL_DEV_WORKSPACE_ROOT:-}" ]; then
+      echo "AUCPL_DEV_WORKSPACE_ROOT is not set" >&2
+      exit 1
+    fi
+
+    exec cargo run --quiet --manifest-path "''${AUCPL_DEV_WORKSPACE_ROOT}/Cargo.toml" --bin aucpl -- "$@"
+  '';
 in
   pkgs.mkShell rec {
     packages =
       [
+        aucpl-dev
         pkgs.rust-analyzer
       ]
       ++ lib.optionals pkgs.stdenv.hostPlatform.isLinux [
@@ -15,4 +24,9 @@ in
     ];
 
     LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+
+    shellHook = ''
+      export AUCPL_DEV_WORKSPACE_ROOT="$(dirname "$(cargo locate-project --workspace --message-format plain)")"
+      eval "$(command aucpl shellinit)"
+    '';
   }
